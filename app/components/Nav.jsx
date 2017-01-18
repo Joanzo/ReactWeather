74f4d49d25fc3e3485362ead79149cdcb96ccd10
Johannes Untoro <jo@venmond.com>
2017-01-18 12:04:28 +0800

    Style top bar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -15,6 +15,48 @@ var Nav = React.createClass({
 });
 */
 
+var Nav = React.createClass({
+  onSearch: function(e) {
+    e.preventDefault();
+    alert('Not yet wired up');
+  },
+  render: function() {
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">
+              React Weather App
+            </li>
+            <li>
+              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+            </li>
+            <li>
+              <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+            </li>
+            <li>
+              <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <form onSubmit={this.onSearch}>
+            <ul className="menu">
+              <li>
+                <input type="search" placeholder="Search weather" />
+              </li>
+              <li>
+                <input type="submit" className="button" value="Get Weather" />
+              </li>
+            </ul>
+          </form>
+        </div>
+      </div>
+    );
+  }
+});
+
+/*
 var Nav = (props) => {
     return (
         <div>
@@ -25,5 +67,5 @@ var Nav = (props) => {
         </div>
     );
 }
-
+*/
 module.exports = Nav;
